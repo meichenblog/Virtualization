7bd0a6a4f4ddabe79a756f508448b50355297109
MeiChen <13316899740@163.com>
2021-05-13 13:48:27 +0800

    Update install_ovirt.sh

--- Ovirt/install_ovirt.sh
+++ Ovirt/install_ovirt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 """
-# 官网iso镜像下载地址：https://resources.ovirt.org/pub/ovirt-4.4/iso/ovirt-node-ng-installer/
+# 官网iso镜像下载地址：
+  https://resources.ovirt.org/pub/ovirt-4.4/iso/ovirt-node-ng-installer/
+# ovirt-4.3.9-.iso镜像地址：
+  https://resources.ovirt.org/pub/ovirt-4.3/iso/ovirt-node-ng-installer/4.3.9-2020031917/el7/
 """
